3d14633172be8574ddc7b6e2cc5c3687891d1350
henriqueherrera <henriqueherrera9@gmail.com>
2019-06-30 12:42:37 -0300

    add sound and repair colision

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@
 #include <chrono>
 #include <thread>
 #include <stdlib.h>
-
+#include <SDL/SDL_mixer.h>
 using namespace std;
 
 SDL_Surface *screen, *imgFly, *imgBackGroung[2], *text, *placar, *imgPerson = NULL;
@@ -34,6 +34,9 @@ SDL_Rect movBackGround[3], movGround[2],rectFly[4], movFly, rectPonto;
 
 SDL_Rect rectBackGroung[2], rectGround, rectPerson[3], movPerson;
 
+Mix_Music *mscMusic = NULL;
+Mix_Chunk *mscFly = NULL;
+Mix_Chunk *mscColision = NULL;
 TTF_Font *font;
 
 Uint32 color, color2, start;
@@ -76,7 +79,23 @@ void setFly() {
     movFly.x = 810;
     movFly.y = 50;
 }
-
+void setSound() {
+    if( Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+	cout<<"SDL_Error: \n"<<SDL_GetError()<<endl;	
+    }
+    
+    mscMusic = Mix_LoadMUS("snd.wav");
+    
+    if( mscMusic == NULL) {
+	cout<<"Couldn't load snd.wav: "<<Mix_GetError()<<endl;
+    }
+    
+    mscFly = Mix_LoadWAV("sndfly.wav");
+    mscColision = Mix_LoadWAV("sndcolision.wav");
+    if( mscFly == NULL || mscColision == NULL ) {
+	cout<<"Couldn't load msc: "<<Mix_GetError()<<endl;
+    }
+}
 void setBackGroung() {
     int movBg[3] = {0, 1280, 2520};
     imgBackGroung[0] = SDL_DisplayFormat(SDL_LoadBMP("fundo.bmp"));
@@ -123,17 +142,23 @@ void blitScreen() {
 string gameOverTxt(int score) {
     rectPonto.x = 200;
     rectPonto.y = 200;
-    string x = "Game Over\nPontos:	"+to_string(score)+"\nPara recomeçar aperte(Enter)\nPara Sair do jogo aperte(Espaço)";
+    string x = "Game Over";
     return x;
 }
+
+void setRectPonto() {
+    rectPonto.x = 200;
+    rectPonto.y = 10;
+}
+
+
 void setGame() {
+    setSound();
     setPerson();
     setBackGroung();
     setFly();
-    rectPonto.x = 300;
-    rectPonto.y = 0;	
+    setRectPonto();	
 }
-
 void mov() {
     movGround[0].x--;
     movGround[1].x--;
@@ -160,7 +185,11 @@ void inits() {
 
 void colision() {
     if((movFly.x >= movPerson.x && movFly.x <= movPerson.x+105) && (( movFly.y>= movPerson.y && movFly.y <= movPerson.y+60 )|| (movFly.y+55 >= movPerson.y && movFly.y <= movPerson.y+60 ))) {
+	Mix_PlayChannel(-1,  mscColision, 0);
 	gameOver = true;
+    }
+    if(movPerson.y >= 520) {
+    	movPerson.y = 520;
     }
 }
 
@@ -184,7 +213,10 @@ void keyBoard() {
 		        score = 0;
 			movFly.x = fly;
 			movFly.y = rand()%550;
-			placar = TTF_RenderText_Solid(font, pontos.c_str(), {210,40,89});
+			setRectPonto();
+		    	placar = TTF_RenderText_Solid(font, pontos.c_str(), {210,40,89});
+		    } else {
+			Mix_PlayChannel(-1,  mscFly, 0);
 		    }
 		}
 		if (event.key.keysym.sym == 13) {
@@ -223,7 +255,6 @@ void isGameOver(){
 	movFly.y = rand()%550;
 	sort = rand()%9+1;
 	pontos = "Pontos: "+to_string(score);
-	cout<<pontos<<endl;
 	placar = TTF_RenderText_Solid(font, pontos.c_str(), {210,40,89});
     }
     if( movFly.y> movPerson.y) {
@@ -238,7 +269,8 @@ void isGameOver(){
     } else {
 	//cout<<gameOverTxt(score)<<endl;
 	//cout<<gameOver<<endl;
-	placar = TTF_RenderText_Solid(font,"Game Over",{210,40,89});
+	string str = gameOverTxt(score);
+	placar = TTF_RenderText_Solid(font,str.c_str(),{210,40,89});
     }
 }
 
@@ -246,8 +278,12 @@ void engine(){
     inits();
     thread  delay(delayBlitPerson);
     setGame();
-  
+    if (Mix_PlayingMusic() == 0) {
+	Mix_PlayMusic(mscMusic, -1);
+    }
+    
     while(running) {
+	cout<<movPerson.y<<endl;
 	start = SDL_GetTicks();
 	keyBoard();
 	colision();
@@ -264,7 +300,8 @@ void freeScreen() {
     SDL_FreeSurface(imgBackGroung[0]);
     TTF_CloseFont(font);
     SDL_Quit(); 
- 
+    Mix_FreeMusic(mscMusic);
+    Mix_FreeChunk(mscFly);
 }
 
 int main (int argc, char ** argcv) {
